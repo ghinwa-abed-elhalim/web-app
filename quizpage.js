d4cd1351a0e911a8e13581210a95a89b16704493
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-21 18:47:08 +0100

    adding quiz title and rendering quiz questions function added

--- quizpage.js
+++ quizpage.js
@@ -1,2 +1,18 @@
 const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 const quizIndex = localStorage.getItem("currentQuizIndex");
+
+if (!currentUser) {
+    window.location.href = "index.html";
+}
+
+const quizForm = document.querySelector(".quiz-form");
+const submitButton = document.querySelector(".submit-btn");
+const scoreParagraph = document.querySelector(".score-result");
+const quizzes = JSON.parse(localStorage.getItem("quizzes") || "[]");
+const selectedQuiz = quizzes[quizIndex];
+
+document.querySelector(".quiz-title").innerText = selectedQuiz.title;
+
+selectedQuiz.questions.forEach(function (questionObj, questionIndex) {
+    const questionContainer = document.createElement("div");
+    questionContainer.classList.add("question-box");
