422f36ecfd3b81e8e2922daf222a8208bff80f28
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-21 18:19:59 +0100

    checking for admin login and finding matching user getting the users input and checking if alreading exists functions and conditional settings added

--- main.js
+++ main.js
@@ -19,10 +19,65 @@ registerTabButton.addEventListener("click", function() {
 
 loginFormElement.addEventListener("submit", function (event) {
     event.preventDefault();
+    
 
     const loginEmail = document.querySelector(".login-email").value;
     const loginPassword = document.querySelector(".login-password").value;
     const storedUsers = localStorage.getItem("users");
     const userArray = storedUsers ? JSON.parse(storedUsers) : [];
+
+    if (loginEmail === "admin@quiz.com" && loginPassword === "admin123") {
+        const adminUser = { name: "Admin", email: loginEmail };
+        localStorage.setItem("currentUser", JSON.stringify(adminUser));
+        window.location.href = "dashboard.html";
+        return;
+    }
     
-})
+    const foundUser = usersArray.find(function (user) {
+        return user.email === loginEmail && user.password === loginPassword;
+    });
+
+    if (foundUser) {
+        localStorage.setItem("currentUser", JSON.stringify(foundUser));
+        window.location.href = "home.html";
+      } else {
+        messageParagraph.innerText = "Invalid credentials! Please try again.";
+      }
+    });
+    
+    
+    registerFormElement.addEventListener("submit", function (event) {
+      event.preventDefault();
+      
+      const registerName = document.querySelector(".register-name").value;
+      const registerEmail = document.querySelector(".register-email").value;
+      const registerPassword = document.querySelector(".register-password").value;
+
+
+      const storedUsers = localStorage.getItem("users");
+      const usersArray = storedUsers ? JSON.parse(storedUsers) : [];
+    
+      const userExists = usersArray.some(function (user) {
+        return user.email === registerEmail;
+      });
+    
+      if (userExists) {
+        messageParagraph.innerText = "User already exists with this email!";
+        return;
+      }
+
+      const newUser = {
+        name: registerName,
+        email: registerEmail,
+        password: registerPassword,
+        scores: []
+      };
+
+      usersArray.push(newUser);
+      localStorage.setItem("users", JSON.stringify(usersArray));
+
+      
+      messageParagraph.innerText = "Registration successful! You can log in now.";
+      registerFormElement.classList.add("hidden");
+      loginFormElement.classList.remove("hidden");
+    });
