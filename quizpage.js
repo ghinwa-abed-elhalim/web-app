624cae10f107c4d63a8a8d75d7668996f0d47485
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-21 19:10:13 +0100

    scoring added

--- quizpage.js
+++ quizpage.js
@@ -25,4 +25,53 @@ selectedQuiz.questions.forEach(function (questionObj, questionIndex) {
     questionObj.options.forEach(function (optionValue) {
         const label = document.createElement("label");
         label.classList.add("option-label");
+        
+        const optionInput = document.createElement("input");
+        optionInput.setAttribute("type", "radio");
+        optionInput.setAttribute("name", "question-" + questionIndex);
+        optionInput.setAttribute("value", optionValue);
+        
+        label.appendChild(optionInput);
+        label.appendChild(document.createTextNode(optionValue));
+        
+        questionContainer.appendChild(label);
     
+    });
+    
+    quizForm.appendChild(questionContainer);
+});
+
+
+submitButton.addEventListener("click", function () {
+    let score = 0;
+    
+    selectedQuiz.questions.forEach(function (questionObj, questionIndex) {
+        const selectedOption = document.querySelector(
+            'input[name="question-' + questionIndex + '"]:checked'
+        );
+        
+        if (selectedOption && selectedOption.value === questionObj.answer) {
+            score += 1;
+        
+        }
+    });
+    
+    const total = selectedQuiz.questions.length;
+    const finalScore = `${score} / ${total}`;
+    scoreParagraph.innerText = `You scored ${finalScore}! 🎉`;
+    
+    const users = JSON.parse(localStorage.getItem("users"));
+    const userIndex = users.findIndex(u => u.email === currentUser.email);
+    
+    if (userIndex !== -1) {
+        users[userIndex].scores.push({
+        quizTitle: selectedQuiz.title,
+        score: finalScore
+    
+    });
+    
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+submitButton.disabled = true;
+});
