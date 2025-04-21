b644a3abcd128cdaac8a61a26ca6c73205cf77b6
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-21 15:59:18 +0100

    variables added

---
+++ main.js
@@ -0,0 +1,7 @@
+const loginTabBtn = document.querySelector(".login-tab");
+const registerTabBtn = document.querySelector(".register-tab");
+const loginForm = document.querySelector(".login-form");
+const registerForm =  document.querySelector("register-form");
+const messageEl = document.querySelector(".message");
+
+
