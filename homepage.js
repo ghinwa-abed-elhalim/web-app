463fb3416921f37a9b2cb171cb03183822d7e6e4
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-21 18:34:13 +0100

    added another variables

--- homepage.js
+++ homepage.js
@@ -1,1 +1,4 @@
 const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+const welcomeMessage = document.querySelector(".welcome-message");
+const quizListContainer = document.querySelector(".quiz-list");
+const logoutButton = document.querySelector(".logout-btn");
