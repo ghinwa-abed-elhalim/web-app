5e6cd821d1e07a2697d9a9271b12a2283b3793d1
Ghinwa Abed Elhalim <ghenwaah77@gmail.com>
2025-04-21 18:39:19 +0100

    foreach function to display the quiz added

--- homepage.js
+++ homepage.js
@@ -52,4 +52,22 @@ if (!currentUser) {
       ]
     }
   ];
+
+
+  if (!localStorage.getItem("quizzes")) {
+    localStorage.setItem("quizzes", JSON.stringify(initialQuizzes));
+  }
   
+  const quizzes = JSON.parse(localStorage.getItem("quizzes"));
+
+  quizzes.forEach(function (quiz, index) {
+    const quizButton = document.createElement("button");
+    quizButton.innerText = quiz.title;
+    quizButton.classList.add("quiz-btn");
+    quizButton.addEventListener("click", function () {
+      // Save the selected quiz index so we know which one to load later
+      localStorage.setItem("currentQuizIndex", index);
+      window.location.href = "quiz.html";
+    });
+    quizListContainer.appendChild(quizButton);
+  });
